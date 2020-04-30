abf6b54c04f96b15e75bd78adab2f749fa19456d
DaoLanhtxd <63581562+DaoLanhtxd@users.noreply.github.com>
2020-05-01 01:20:28 +0700

    tuần 9 + 10

--- sv.py
+++ sv.py
@@ -10,6 +10,7 @@ def start_server():
    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
    soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
    print("Socket created")
+   arr=[]
    try:
       soc.bind((host, port))
    except:
@@ -19,14 +20,46 @@ def start_server():
    print("Socket now listening")
    # infinite loop- do not reset for every requests
    while True:
+      test=1
+      file=open("online.log","r+")
       connection, address = soc.accept()
       ip, port = str(address[0]), str(address[1])
-      print("Connected with " + ip + ":" + port)
-      try:
-         Thread(target=clientThread, args=(connection, ip, port)).start()
-      except:
-         print("Thread did not start.")
-         traceback.print_exc()
+      count=0
+      while True:
+         line=file.readline()
+         line=line.replace("\n","")
+         if not line:break
+         else:
+            count=count+1
+            if(line==ip):
+               data=("429 Too Many Requests")
+               connection.sendall(data.encode())
+               f1=open("access.log","w")
+               f1.write(ip+"\t"+port+"\t"+"429 Too Many Requests\n")
+               f1.close()
+               connection.close()
+               test=0
+      if(count>=10):
+         data=("503 Service Unavailable")
+         connection.sendall(data.encode())
+         f1=open("access.log","w")
+         f1.write(ip+"\t"+port+"\t"+"503 Service Unavailable\n")
+         f1.close()
+         connection.close()
+         test=0
+      file.close()
+      if(test==1):
+         print("Connected with " + ip + ":" + port)
+         file=open("online.log","w")
+         file.write(ip+"\n")
+         data=("Connected")
+         connection.sendall(data.encode())
+         file.close()
+         try:
+            Thread(target=clientThread, args=(connection, ip, port)).start()
+         except:
+            print("Thread did not start.")
+            traceback.print_exc()
    soc.close()
 def switch(i):
         switcher={               
@@ -55,6 +88,23 @@ def clientThread(connection, ip, port, max_buffer_size = 5120):
                 except:
                     data='Invalid'
             connection.sendall(data.encode())
+    file=open("access.log","w")
+    file.write(ip+"\t"+port+"\t"+"Closed by Client\n")
+    file.close()
+    arr=[]
+    file=open("online.log","r+")
+    while True:
+         line=file.readline()
+         line=line.replace("\n","")
+         if not line:break
+         else:
+            arr.append(line)
+    file.close()
+    arr.remove(ip)
+    file=open("online.log","w")
+    for i in arr:
+         file.write(i+"\n")
+    file.close()
     connection.close()
 def receive_input(connection, max_buffer_size):
    client_input = connection.recv(max_buffer_size)
